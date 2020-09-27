bf9794e4e7637bd5029b5265a65b319b038211f6
j_and <j_andrespuk@hotmail.com>
2020-09-27 10:53:24 +0300

    - added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductTableComponent } from './product-table/product-table.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoriesComponent,
+    ProductTableComponent,
+    NotFoundComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
