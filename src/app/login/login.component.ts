c05579a5df6b0ba3f93fb6fd686e8e31953d7bb0
Eric Ngako <ericnk1@live.com>
2020-09-30 21:20:27 +0300

    login component added

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Login} from '../models/Login';
+import {LoginService} from '../services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private snacbar: MatSnackBar, private formBuilder: FormBuilder, private loginService: LoginService) {
+  }
+
+  loginGroup: FormGroup;
+
+  ngOnInit(): void {
+    this.loginGroup = this.formBuilder.group({
+      username: '',
+      password: ''
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  login() {
+    const login = new Login(this.loginGroup.get('username').value, this.loginGroup.get('password').value);
+    this.loginService.validateLogin(login).subscribe(
+      value => window.location.assign('/index.html'),
+      error => {
+        this.snacbar.open(error.error.message.concat(error.error.details[0]), 'close', {
+          duration: 6000,
+          panelClass: 'snack-error-message'
+        });
+      }
+    );
+
+  }
+}
