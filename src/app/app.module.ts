0dd6ffa6155aebf39a87eb5f96a021490c34c13c
Preche Florian <amourdreche2@gmail.com>
2020-10-03 16:26:32 +0300

    FlexLayoutModule errors solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
-import {CategoriesComponent} from './categories/categories.component';
-import {ProductTableComponent} from './product-table/product-table.component';
-import {NotFoundComponent} from './not-found/not-found.component';
-import {SearchBarComponent} from './search-bar/search-bar.component';
-import {WelcomeComponent} from './welcome/welcome.component';
-import {AppRoutingModule} from './app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductTableComponent } from './product-table/product-table.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import 'hammerjs';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
-import {MatMenuModule} from '@angular/material/menu';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-
+import {MatInputModule} from '@angular/material/input';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -46,7 +43,6 @@ import { SignupComponent } from './signup/signup.component';
     MatToolbarModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule,
     FormsModule,
     MatIconModule,
     MatSliderModule,
@@ -54,10 +50,11 @@ import { SignupComponent } from './signup/signup.component';
     MatCheckboxModule,
     MatInputModule,
     MatCardModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
